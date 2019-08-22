db4988c508201ad7665773927f77e71e80f2d6a6
Rajan Aggarwal <rajansien@gmail.com>
2019-08-23 01:20:05 +0530

    Add variables to the code

--- instances/simple/main.tf
+++ instances/simple/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "simple_instance" {
   # ubuntu 18.04 t2.micro free tier
-  ami = "ami-009110a2bf8d7dd0a"
-  instance_type = "t2.micro"
+  ami = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
 
   tags {
     Name = "simple_instance"
--- instances/simple/variables.tf
+++ instances/simple/variables.tf
@@ -1,7 +1,21 @@
 variable "region" {
   default = "ap-south-1"
+  description = "The region of the data-center"
 }
 
 variable "profile" {
+  default = "default"
   description = "AWS credentials profile as per user declaration"
 }
+
+variable "ami_id" {
+  default = "ami-009110a2bf8d7dd0a" # ubuntu 18.04 for ap-south-1
+  description = "The image id for the ec2 instance image"
+}
+
+variable "instance_type" {
+  default = "t2.micro" # free-tier 
+  description = "The type of instance for the deployment"
+}
+
+
