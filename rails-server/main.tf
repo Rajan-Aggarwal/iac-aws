51ad10a528dc5d2f344d4d4999976a4fde0a06f1
Rajan Aggarwal <rajansien@gmail.com>
2019-08-23 11:52:16 +0530

    Add provisioner via ssh

--- rails-server/main.tf
+++ rails-server/main.tf
@@ -2,11 +2,29 @@ resource "aws_instance" "rails-instance" {
   # Ubuntu 18.04 AMI; t2.micro (free-tier)
   ami = "${data.aws_ami.ubuntu-18.04}"
   instance_type = "${var.instance_type}"
+  key_name = "${aws_key_pair.ruby_key.key_name}"
 
   security_groups = [
     "${aws_security_group.allow_ssh}",
     "${aws_security_group.allow_outbound}",
   ]
+
+  # install rails
+
+  provisioner "remote-executioner" {
+    inline = [
+      "command curl -sSL https://rvm.io/mpapis.asc | gpg --import -",
+      "\\curl -sSL https://get.rvm.io | bash -s stable --rails",
+    ]
+
+    connection = {
+      type = "ssh"
+      user = "ubuntu" # default user in every aws ec2 instance
+      private_key = "${file("/home/rajan/ruby_key.pub")}"
+    }
+  } 
+
+  # metadata
 
   tags = {
       Name = "Rails instance"
